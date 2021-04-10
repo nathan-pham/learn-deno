d6411831598a4bd709b7558e55a9fe3787202d4a
Nathan Pham <nathanpham.me@gmail.com>
2021-04-09 19:07:43 -0700

    gitignore

--- deno-basics/permissions.ts
+++ deno-basics/permissions.ts
@@ -1,5 +1,13 @@
-// --allow-read
+// deno run --flags file.ts
+// --allow-read or --allow-read=C:/Users/nathan-pham/Desktop/projects/learn-deno
 console.log("current working directory:", Deno.cwd())
 console.log("read text file", await Deno.readTextFile("./read.txt"))
 
+// --allow-write
 Deno.writeTextFile("./write.txt", "1 2 3")
+
+// --allow-env
+Deno.env.get("EXAMPLE_PROPERTY")
+
+// --allow-net or --allow-net=exact-domains
+console.log(await fetch("https://jsonplaceholder.typicode.com/todos/1").then(res => res.json()))
