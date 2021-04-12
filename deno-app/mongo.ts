0af020be4d0d4214e2b8ac95fc22fb7932b907b6
Nathan Pham <nathanpham.me@gmail.com>
2021-04-12 10:47:27 -0700

    init models

--- deno-app/mongo.ts
+++ deno-app/mongo.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from "./package.ts"
 
 const client = new MongoClient()
-await client.connectWithUri(`mongodb+srv://deno_survey:${Deno.env.get("MONGO_PASSWORD")}@cluster0.n1jw5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+await client.connect(`mongodb+srv://deno_survey:${Deno.env.get("MONGO_PASSWORD")}@cluster0.n1jw5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
 
 const db = client.database("deno_survey")
 const usersCollection = db.collection("users")
